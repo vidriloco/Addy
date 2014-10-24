a185269b860e87c9eed390bad91074e5fc2c9290
Alejandro Cruz Paz <vidriloco@gmail.com>
2014-10-24 17:27:11 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AddyNovi::Application.config.secret_key_base = '7ae1115cc45b12ebc0175f67dd7ce2dc2c0c1a150b0c32ac7c5efce95626d6500f721d06e6f3b7d7970a04a282063ed7fefd52a26d3e035d53b4bf4fdd29cc41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AddyNovi::Application.config.session_store :cookie_store, key: '_addy_novi_session'
